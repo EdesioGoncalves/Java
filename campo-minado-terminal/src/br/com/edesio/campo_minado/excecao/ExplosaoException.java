71aebfbd0ca9bf21ac84b0aabcb0409abfda039b
Edesio Goncalves <edesio_goncalves@hotmail.com>
2021-04-21 12:11:52 -0300

    campo-minado usando terminal v.1

---
+++ campo-minado-terminal/src/br/com/edesio/campo_minado/excecao/ExplosaoException.java
@@ -0,0 +1,5 @@
+package br.com.edesio.campo_minado.excecao;
+
+public class ExplosaoException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+}
---
+++ campo-minado-terminal/src/br/com/edesio/campo_minado/excecao/SairException.java
@@ -0,0 +1,5 @@
+package br.com.edesio.campo_minado.excecao;
+
+public class SairException extends RuntimeException{
+	private static final long serialVersionUID = 1;
+}
