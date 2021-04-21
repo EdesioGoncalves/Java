afbd7303a5ab960f17b0421032fcdb6078230201
Edesio Goncalves <edesio_goncalves@hotmail.com>
2021-04-21 12:06:40 -0300

    Dica na tela principal sobre configurações da aplicação

--- campo-minado/src/br/com/edesio/campo_minado/Aplicacao.java
+++ campo-minado/src/br/com/edesio/campo_minado/Aplicacao.java
@@ -5,7 +5,8 @@ import br.com.edesio.campo_minado.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		//Aqui você altera as configurações do jogo (coluna, linha, bombas)
+		Tabuleiro tabuleiro = new Tabuleiro(8,8,6);
 		new TabuleiroConsole(tabuleiro);
 	}
 }
