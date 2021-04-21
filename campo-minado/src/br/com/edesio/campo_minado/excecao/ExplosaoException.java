d4b72b400d4b1d25b4bbf3ffd9e3275892afe933
Edésio Gonçalves <60361061+EdesioGoncalves@users.noreply.github.com>
2021-04-21 12:13:52 -0300

    Delete campo-minado directory

--- campo-minado/src/br/com/edesio/campo_minado/excecao/ExplosaoException.java
+++
@@ -1,5 +0,0 @@
-package br.com.edesio.campo_minado.excecao;
-
-public class ExplosaoException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-}
--- campo-minado/src/br/com/edesio/campo_minado/excecao/SairException.java
+++
@@ -1,5 +0,0 @@
-package br.com.edesio.campo_minado.excecao;
-
-public class SairException extends RuntimeException{
-	private static final long serialVersionUID = 1;
-}
